7fd66253f58f90a83d18191c093afbb79e398cf3
DanCardoso15 <dancardoso15@hotmail.com>
2024-12-10 14:43:55 -0300

    adicionar livro

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
   const [livros, setLivros] = useState([]);
+  const { handleSubmit, register, reset } = useForm();
 
   async function loadData() {
     const resposta = await fetch("http://localhost:3000/livros");
@@ -12,7 +14,7 @@ function App() {
 
   async function excluirLivro(id) {
     await fetch(`http://localhost:3000/livros/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
     loadData();
   }
@@ -22,9 +24,19 @@ function App() {
     await fetch(`http://localhost:3000/livros/${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ titulo })
+      body: JSON.stringify({ titulo }),
     });
     loadData();
+  }
+
+  async function salvarLivro(dados) {
+    await fetch("http://localhost:3000/livros", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dados),
+    });
+    loadData();
+    reset();
   }
 
   useEffect(() => {
@@ -34,19 +46,55 @@ function App() {
   return (
     <div>
       <h1>Livros</h1>
-
+      <form onSubmit={handleSubmit(salvarLivro)}>
+        <div>
+          <label htmlFor="titulo">Titulo</label> <br />
+          <input type="text" id="titulo" {...register("titulo")} />
+        </div>
+        <div>
+          <label htmlFor="autor">Autor</label> <br />
+          <input type="text" id="autor" {...register("autor")} />
+        </div>
+        <div>
+          <label htmlFor="paginas">Paginas</label> <br />
+          <input type="text" id="paginas" {...register("paginas")} />
+        </div>
+        <div>
+          <label htmlFor="categoria">Categoria</label> <br />
+          <select id="categoria" {...register("categoria")}>
+            <option value="Ação">Ação</option>
+            <option value="Terror">Terror</option>
+            <option value="Tecnologia">Tecnologia</option>
+            <option value="Romance">Romance</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="dataPublicacao">Data de Publicacao</label> <br />
+          <input
+            type="text"
+            id="dataPublicacao"
+            {...register("dataPublicacao")}
+          />
+        </div>
+        <div>
+          <label htmlFor="isbn">ISBN</label> <br />
+          <input type="text" id="isbn" {...register("isbn")} />
+        </div>
+        <button>Adicionar</button>
+      </form>{" "}
+      <br />
       <table>
         <tbody>
-          {livros.map(livro => (
+          {livros.map((livro) => (
             <tr key={livro.id}>
-             <td>{livro.titulo}</td>
-             <td>{livro.autor}</td>
-             <td>{livro.paginas}</td>
-             <td>{livro.categoria}</td>
-             <td>
-              <button onClick={() => editarLivro(livro.id)}>Editar</button>
-             </td>
-             <button onClick={() => excluirLivro(livro.id)}>Excluir</button>
+              <td>{livro.titulo}</td>
+              <td>{livro.autor}</td>
+              <td>{livro.paginas}</td>
+              <td>{livro.categoria}</td>
+              <td>
+                <button onClick={() => editarLivro(livro.id)}>Editar</button>
+              </td>
+              <button onClick={() => excluirLivro(livro.id)}>Excluir</button>
             </tr>
           ))}
         </tbody>
@@ -55,4 +103,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
